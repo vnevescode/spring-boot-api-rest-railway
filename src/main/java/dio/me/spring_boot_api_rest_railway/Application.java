dbfb65900c42f02fa7e1df0c06e50c6f755ee402
Victor Neves <victorneves15@gmail.com>
2025-03-18 18:07:48 -0300

    Alteração no build gradle

--- src/main/java/dio/me/spring_boot_api_rest_railway/Application.java
+++ src/main/java/dio/me/spring_boot_api_rest_railway/Application.java
@@ -1,12 +1,27 @@
 package dio.me.spring_boot_api_rest_railway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+
 	public static void main(String[] args) {
+
+		// Log Environment Variables BEFORE starting Spring context
+		log.info("DATABASE_URL: {}", System.getenv("DATABASE_URL"));
+		log.info("DATABASE PGHOST: {}", System.getenv("PGHOST"));
+		log.info("PGUSER: {}", System.getenv("PGUSER"));
+		log.info("PGPASSWORD: {}", System.getenv("PGPASSWORD"));
+		log.info("PGPORT: {}", System.getenv("PGPORT"));
+		log.info("PORT: {}", System.getenv("PORT"));
+
+
 		SpringApplication.run(Application.class, args);
 	}
 

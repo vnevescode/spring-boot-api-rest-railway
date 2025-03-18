ef4bcc00096eca88d2b73d3832f0f29d9fe73cb4
Victor Neves <victorneves15@gmail.com>
2025-03-18 18:35:52 -0300

    BigFix: CORS

--- src/main/java/dio/me/spring_boot_api_rest_railway/Application.java
+++ src/main/java/dio/me/spring_boot_api_rest_railway/Application.java
@@ -2,9 +2,12 @@ package dio.me.spring_boot_api_rest_railway;
 
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition( servers = { @Server(url= "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
